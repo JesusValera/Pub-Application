5132676f25ae1d0f3fec83a5a76ae3d11c0985f3
Jesus Valera <jesus1902@outlook.com>
2018-09-25 17:02:53 +0200

    Vid 52. Access control.

--- src/AppBundle/Controller/ManagementTapaController.php
+++ src/AppBundle/Controller/ManagementTapaController.php
@@ -14,6 +14,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/management")
+ */
 class ManagementTapaController extends Controller
 {
     /**
